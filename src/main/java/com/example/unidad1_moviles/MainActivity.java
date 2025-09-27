570f4ddf50b508c4e7dac90bd9d8b089510c8ba8
JuanPatinoPlayon <camilo.patino@est.iudigital.edu.co>
2025-09-27 00:37:43 -0500

    Ciclo de vida implementado en las activity

--- src/main/java/com/example/unidad1_moviles/MainActivity.java
+++ src/main/java/com/example/unidad1_moviles/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.unidad1_moviles;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         TextView tvBienvenido = findViewById(R.id.tvBienvenido);
         tvBienvenido.setText("¡Bienvenido a la aplicación!");
     }
+
+    @Override protected void onStart() { super.onStart(); Log.d("CICLO_VIDA", "MainActivity - onStart"); }
+    @Override protected void onResume() { super.onResume(); Log.d("CICLO_VIDA", "MainActivity - onResume"); }
+    @Override protected void onPause() { super.onPause(); Log.d("CICLO_VIDA", "MainActivity - onPause"); }
+    @Override protected void onStop() { super.onStop(); Log.d("CICLO_VIDA", "MainActivity - onStop"); }
+    @Override protected void onRestart() { super.onRestart(); Log.d("CICLO_VIDA", "MainActivity - onRestart"); }
+    @Override protected void onDestroy() { super.onDestroy(); Log.d("CICLO_VIDA", "MainActivity - onDestroy"); }
 }
+
 
 
